bdb8eb345074711fa79ed2e488e528d1233c7cbb
LukasLawler <headfirstluke377@gmail.com>
2021-04-26 19:47:34 -0500

    Initial check-in of module TagHelperLawler

---
+++ TagHelperLawler/Models/MyButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace TagHelperLawler.Models
+{
+    public class MyButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("type", "button");
+            string newClasses = "btn btn-outline-danger";
+            string oldClasses = output.Attributes["class"]?.Value?.ToString();
+            string classes = (string.IsNullOrEmpty(oldClasses)) ?
+                newClasses : $"{oldClasses} {newClasses}";
+            output.Attributes.SetAttribute("class", classes);
+        }
+    }
+}
